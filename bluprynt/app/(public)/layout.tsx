705ee048bfdbe18fdd7cd80364c097d70bdac104
Akanksha Duvvuri <akanksha.duvvuri16@gmail.com>
2026-05-16 01:12:08 +0530

    changes

--- bluprynt/app/(public)/layout.tsx
+++ bluprynt/app/(public)/layout.tsx
@@ -1,5 +1,4 @@
 import { CADNavbar } from "@/app/components/CADNavbar";
-import { CADStatusBar } from "@/app/components/CADStatusBar";
 import  CADCrosshair  from "../components/CADCrosshair";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
@@ -8,7 +7,6 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
       <CADCrosshair />
       <CADNavbar />
       <main>{children}</main>
-      <CADStatusBar />
     </>
   );
 }
